ae9666037b5808e5b7e0f0c6217aa2c9faf3c2ab
Jack <jack@lusher.digital>
2025-09-10 00:06:29 +0100

    Remove old app directory after package rename
    - Deleted entire app/ directory
    - All functionality moved to minecraft_server_automation/
    - Package rename from 'app' to 'minecraft_server_automation' complete
    - All imports updated to use package:minecraft_server_automation/ directive

--- app/lib/models/cpu_architecture.dart
+++
@@ -1,9 +0,0 @@
-/// Enum representing CPU architecture types
-enum CpuArchitecture {
-  shared('shared', 'Shared CPU'),
-  dedicated('dedicated', 'Dedicated CPU');
-
-  const CpuArchitecture(this.value, this.displayName);
-  final String value;
-  final String displayName;
-}
