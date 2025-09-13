868da39c8ae3852dfe8b75e4df5acdb2f0786bb1
Jack <jack@lusher.digital>
2025-09-13 09:13:42 +0100

    Update ApiKeyManagementBanner to use LoadingIndicator
    - Replace duplicated CircularProgressIndicator with LoadingIndicator.small
    - Maintain consistent loading state appearance in banners

--- minecraft_server_automation/lib/widgets/api_key_management_banner.dart
+++ minecraft_server_automation/lib/widgets/api_key_management_banner.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'dart:io';
 import 'package:minecraft_server_automation/providers/auth_provider.dart';
 import 'api_key_input_dialog.dart';
+import 'package:minecraft_server_automation/common/widgets/feedback/loading_indicator.dart';
 
 class ApiKeyManagementBanner extends StatefulWidget {
   const ApiKeyManagementBanner({super.key});
@@ -324,7 +325,7 @@ class _ApiKeyManagementBannerState extends State<ApiKeyManagementBanner> {
       return const SizedBox(
         width: 16,
         height: 16,
-        child: CircularProgressIndicator(strokeWidth: 2),
+        child: LoadingIndicator.small(),
       );
     }
 
